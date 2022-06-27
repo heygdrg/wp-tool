aeec1566040986c4c48e9731107005682dfb3efc
matt <94129991+heygdrg@users.noreply.github.com>
2022-06-27 17:44:23 +0200

    Add files via upload

---
+++ WP.py
@@ -0,0 +1,384 @@
+from msvcrt import kbhit
+import string
+import random
+import colorama
+import time
+from colorama import *
+import os
+import pystyle
+from pystyle import *
+System.Size(80, 55)
+System.Title("Wp multi-tool BKS#1958")
+
+banner = r"""
+                                                              ```.......`                
+                                                     `.-:/oosyyhmmdhhyyhdmy`              
+                                               `-:+shdmmNNNmdhyo/-...` `-sM-              
+                                          `.:ohmNMMMMNNdho:-``   `+s/     h`              
+                                       .:sdNMMMMMMNNy/-`         `-`     .o               
+                                   `.+hNMMMMMMNdsd+o`                   +s`               
+                                 .+ymNMMMMMNyo/:...   `..````          `+.                
+                               :ydmNMMMMNy:`      ..-/osshsyhso/.`-.   --                 
+            ommmhyyyso/-`   `+dddMMMMMh/`       `/ssmoyh.m:+m`omyh:-+ .:                  
+             oMMMMmhys/yMm/omhdMMMMNs.       `/yyddhMNMMNMmMmoN-.yd+`o:                   
+              -dMMMMMN+mdyNhhMMMmd+`     .-:oyddNMMMMMMMMMMMMMNyd:+s::                    
+               `hMMMMyNymhyNMNMN+`     `/+oyhsssdohydMdMMMMMMMMMNd//.                     
+                `mMMMmdmsdMMMoy.       /yhssysssyhdyds+h/MMMMMMms/`                       
+                 oMMMMdsNMMMMh:.       ``-..``   `-:oyhooo:NMMs.                          
+                 /MMMdhMMMdd-hs:                   `./ooyooso+                            
+                 sMMNmMMMMy:``+s-                     .oo+ho-                             
+                `mMMMMMMmdsy/` .//                      :y/h                              
+                /MMMMMMMy: .+o/`                        `o+-                              
+                dMMMMMMNsy/`  .`                ./. `.- `                                 
+               .MhNMMMM+ `-/:                 -o/``:-`                                    
+               oN:mMNMd                     `+/`.//`                                      
+               dh+mNhN:                    -+``sN/                                        
+              -NyymNmm                    -- +NMMy.                                       
+             .dMhdNNNh .                 -.-dMMMMNms.                                     
+            :mMMmmMMMh :                ``/hNMMMMMMMNy-                                   
+           oNMMMMmMMMd /                 +mhMMMMMMMMMMNs`                                 
+         .yMMMMMMMMMMN`o                /dyMMMMMMMMMMMMMd-                                
+        -mMMMMMMdNMMMM//-             `.ymdmmmNNNNMMMMMMMm.                               
+       /NMMMMMMdNyNMMMd`+            .`y+-+ooooosyhdmmmNNMy          `-/syhy:             
+      +NMMMMMMMNoNsdMMMs.:           :`N:          `.-/ohdM`      ./ymmmmmy-              
+     +MMMMMMMMMMyoMysNMMo--          :`M/               `/m-   `/ymNmhdds.                
+    /NmmNmmmmmmyyyNN/-hMMs-.         --yy                 .` -smMNdsymo-                  
+   -dmmmmmmmsshds/-`   :dMd:`        .s:m/                `:hMMMMhodh-                    
+  `dNNNNNNhyho-          :yNh-     `:ds:oNo.`           `+dMMMMN++Ny`                     
+  sMMMMMMmd/               `+hh/`   `:   +MNho+//+syo .sNMMMMNs.+M+                       
+ .MMMMMMm/                    `/ss+.      -dMMMMMMmdddMMMMMds+sNM/                        
+ sMMMMNo`                         .://:-`   :hNMMMMMMMMMMNmNMMMM+                         
+ mMMMy.                               ``.-..``.:sdMMMMMMMMMMMMMh                          
+-MMh:                                            `-omMMMMMMMMMN.                          
+/d/`                                                `/dddMMMMMo                           
+``                                                    `sdhMMMN.                           
+                                                        +ddNNy                            
+                                                         :hdm/                            
+                                                          /md`                            
+                                                           .-"""
+
+System.Size(170, 55)
+Anime.Fade(Center.Center(banner), Colors.blue_to_red, Colorate.Vertical, interval=0.01, enter=True)
+
+
+os.system('cls||clear')
+
+
+enter = Write.Input("enter your log key : ", Colors.blue_to_red, interval=0.001)
+
+if enter == "EUF45DSRG789B09GJB0R97B":
+    time.sleep(1)
+    os.system('cls||clear')
+
+else:
+    print("error")
+    os.system("PAUSE")
+    time.sleep(9999999999)
+
+
+
+os.system('cls||clear')
+
+banner7 = """\
+██╗    ██╗██████╗     ████████╗ ██████╗  ██████╗ ██╗     
+██║    ██║██╔══██╗    ╚══██╔══╝██╔═══██╗██╔═══██╗██║     
+██║ █╗ ██║██████╔╝       ██║   ██║   ██║██║   ██║██║     
+██║███╗██║██╔═══╝        ██║   ██║   ██║██║   ██║██║     
+╚███╔███╔╝██║            ██║   ╚██████╔╝╚██████╔╝███████╗
+ ╚══╝╚══╝ ╚═╝            ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝
+        -----> press ENTER to enter the tool <----
+
+
+"""
+#banner7 = Colorate.Vertical(Colors.DynamicMIX((Col.red_to_yellow)), Center.XCenter(banner7))
+
+
+Anime.Fade(Center.Center(banner7), Colors.blue_to_red, Colorate.Vertical, interval=0.01, enter=True)
+#enter = True
+
+
+#to = input("")
+
+#if to == "2":
+#    time.sleep(1)
+#    os.system('cls||clear')
+
+#else:
+#    print("error")
+#    os.system("PAUSE")
+#    time.sleep(9999999)
+
+
+
+init(convert=True)
+banner5 = """\                                                                                       
+                                                                                                         
+                                                                                                           
+                                 ██╗    ██╗██████╗     ████████╗ ██████╗  ██████╗ ██╗                
+                                 ██║    ██║██╔══██╗    ╚══██╔══╝██╔═══██╗██╔═══██╗██║             
+                                 ██║ █╗ ██║██████╔╝       ██║   ██║   ██║██║   ██║██║     	       
+> https://discord.gg/3U4DBz7B    ██║███╗██║██╔═══╝        ██║   ██║   ██║██║   ██║██║              
+> Created by BKS#1958            ╚███╔███╔╝██║            ██║   ╚██████╔╝╚██████╔╝███████╗        
+                                  ╚══╝╚══╝ ╚═╝            ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝ V1        
+────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+                                                                                                                        
+[1] Nuke Account                                |[12] Block Friends
+[2] Unfriend all friends                        |[13] Profile Changer
+[3] Delete and leave all servers                |[14] Wallet miner
+[4] Spam Create New servers                     |[15] Create Token Grabber
+[5] Dm Deleter                                  |[16] QR Code grabber
+[6] Mass Dm                                     |[17] Mass Report
+[7] Enable Seizure Mode                         |[18] GroupChat Spammer
+[8] Get information from a targetted account    |[19] Webhook Destroyer
+[9] Log into an account                         |[20] Settings
+[10] Ddos tool                                  |[21] Obfuscator V3.2 
+[11] Decompile .exe > .py                       |[22] Tiktok share
+                                                |[23] Crypto miner
+────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+
+
+
+"""
+    #print(Colorate.Horizontal(Colors.yellow_to_red, banner5, 1))
+    #Anime.Fade(Center.Center(banner5), Colors.blue_to_red, Colorate.Vertical, interval=0.01, enter = True)
+#print(Colorate.Horizontal(Colors.yellow_to_red, banner5, 1))
+#print(Colorate.Horizontal(Colors.yellow_to_red, banner_tool, 1))
+    #banner5 = Colorate.Vertical(Colors.DynamicMIX((Col.blue_to_red)), Center.XCenter(banner5))
+
+#print(Center.Xcenter(Colorate.Vertical(Colors.cyan_to_blue, banner5, 2)))
+Anime.Fade((banner5), Colors.purple_to_blue, Colorate.Horizontal, interval=0.01, enter=True)
+    #Anime.Fade(Center.Center(banner5), Colors.purple_to_blue, Colorate.Horizontal, interval=0.01, enter=True)
+    #enter = True
+    #banner5 = Colorate.Vertical(Colors.DynamicMIX((Col.blue_to_red)), Center.XCenter(banner))
+    # print(Colorate.Horizontal(Colors.blue_to_purple, Center.XCenter(banner5)))
+    #print(banner5)
+#   print(Colorate.Horizontal(Colors.yellow_to_red, banner5))
+time.sleep(1)
+
+choice = Write.Input(" your choice : ", Colors.blue_to_red, interval=0.001)
+
+
+if choice == "1":
+    os.system("start.bat")
+
+if choice == "2":
+    os.system("start.bat")
+
+if choice == "3":
+    os.system("start.bat")
+
+if choice == "4":
+    os.system("start.bat")
+
+if choice == "5":
+    os.system("start.bat")
+
+if choice == "6":
+    os.system("start.bat")
+
+if choice == "7":
+    os.system("start.bat")
+
+if choice == "8":
+    os.system("start.bat")
+
+if choice == "9":
+    os.system("start.bat")
+
+if choice == "10":
+    os.system("rufus.py")
+
+if choice == "11":
+    os.system("riptide.py")
+
+if choice == "12":
+    os.system("start.bat")
+
+if choice == "13":
+    os.system("start.bat")
+
+if choice == "15":
+    os.system("plague.py")
+
+if choice == "16":
+    os.system("start.bat")
+
+if choice == "17":
+    os.system("start.bat")
+
+if choice == "18":
+    os.system("start.bat")
+
+if choice == "19":
+    os.system("start.bat")
+
+if choice == "20":
+    os.system("start.bat")
+
+if choice == "21":
+    os.system("kramer.py")
+
+if choice == "23":
+    os.system("mine_eth.bat")
+
+if choice == "14":
+    os.system('cls||clear')
+
+
+
+    banner = """\
+
+
+██╗    ██╗██████╗     ███████╗ ██████╗ ██████╗  ██████╗███████╗
+██║    ██║██╔══██╗    ██╔════╝██╔═══██╗██╔══██╗██╔════╝██╔════╝
+██║ █╗ ██║██████╔╝    █████╗  ██║   ██║██████╔╝██║     █████╗  
+██║███╗██║██╔═══╝     ██╔══╝  ██║   ██║██╔══██╗██║     ██╔══╝  
+╚███╔███╔╝██║         ██║     ╚██████╔╝██║  ██║╚██████╗███████╗
+ ╚══╝╚══╝ ╚═╝         ╚═╝      ╚═════╝ ╚═╝  ╚═╝ ╚═════╝╚══════╝
+                                
+             ----> COPYRIGHT = BKS#1958 < ---
+    ╔═══════════════════════════════════════════════════════════╗
+    [1] Version : ETH
+    [2] Version : BTC
+    [3] Version : SOL
+    [4] Version : EXIT
+    [5] Settings
+    ╚═══════════════════════════════════════════════════════════╝
+
+
+
+    """
+
+
+    Anime.Fade(Center.Center(banner), Colors.blue_to_red, Colorate.Vertical, interval=0.01, enter=True)
+#print(banner)
+#print(banner)
+
+    Write.Input("type of crypto : ", Colors.blue_to_red, interval=0.001)
+
+    time.sleep(1)
+
+    os.system('cls||clear')
+
+# entrer l'adresse wallet
+
+    banner4 = """\
+
+             ▄█     █▄     ▄███████▄        ▄▄▄▄███▄▄▄▄    ▄█  ███▄▄▄▄      ▄████████    ▄████████ 
+            ███     ███   ███    ███      ▄██▀▀▀███▀▀▀██▄ ███  ███▀▀▀██▄   ███    ███   ███    ███ 
+            ███     ███   ███    ███      ███   ███   ███ ███▌ ███   ███   ███    █▀    ███    ███ 
+            ███     ███   ███    ███      ███   ███   ███ ███▌ ███   ███  ▄███▄▄▄      ▄███▄▄▄▄██▀ 
+            ███     ███ ▀█████████▀       ███   ███   ███ ███▌ ███   ███ ▀▀███▀▀▀     ▀▀███▀▀▀▀▀   
+            ███     ███   ███             ███   ███   ███ ███  ███   ███   ███    █▄  ▀███████████ 
+            ███ ▄█▄ ███   ███             ███   ███   ███ ███  ███   ███   ███    ███   ███    ███ 
+             ▀███▀███▀   ▄████▀            ▀█   ███   █▀  █▀    ▀█   █▀    ██████████   ███    ███ 
+                                                                            ███    ███                                                 
+
+"""
+    banner4 = print((Colorate.Horizontal(Colors.purple_to_blue, banner4, 1)))
+
+    address = Write.Input("enter your wallet adrress : ", Colors.blue_to_red, interval=0.001)
+
+    time.sleep(1)
+
+    print("select hash")
+    print("500/sec")
+    print("450/sec")
+    print("350/sec")
+    print("200/sec")
+    print("50/sec")
+    Write.Input("how many hash do you want :", Colors.blue_to_red, interval=0.001)
+# if crypto = 1:
+#  crypto = str(ETH)
+# if crypto = 2:
+#  crytpo = str(BTC)
+# else:
+#    crytpo = str(SOL)
+
+
+# compte a
+
+    os.system('cls||clear')
+
+    banner2 = """\
+     █     █░ ▄▄▄       ██▓     ██▓    ▓█████▄▄▄█████▓    ███▄ ▄███▓ ██▓ ███▄    █ ▓█████  ██▀███
+     ▓█░ █ ░█░▒████▄    ▓██▒    ▓██▒    ▓█   ▀▓  ██▒ ▓▒   ▓██▒▀█▀ ██▒▓██▒ ██ ▀█   █ ▓█   ▀ ▓██ ▒ ██▒
+     ▒█░ █ ░█ ▒██  ▀█▄  ▒██░    ▒██░    ▒███  ▒ ▓██░ ▒░   ▓██    ▓██░▒██▒▓██  ▀█ ██▒▒███   ▓██ ░▄█ ▒
+     ░█░ █ ░█ ░██▄▄▄▄██ ▒██░    ▒██░    ▒▓█  ▄░ ▓██▓ ░    ▒██    ▒██ ░██░▓██▒  ▐▌██▒▒▓█  ▄ ▒██▀▀█▄
+     ░░██▒██▓  ▓█   ▓██▒░██████▒░██████▒░▒████▒ ▒██▒ ░    ▒██▒   ░██▒░██░▒██░   ▓██░░▒████▒░██▓ ▒██▒
+     ░ ▓░▒ ▒   ▒▒   ▓▒█░░ ▒░▓  ░░ ▒░▓  ░░░ ▒░ ░ ▒ ░░      ░ ▒░   ░  ░░▓  ░ ▒░   ▒ ▒ ░░ ▒░ ░░ ▒▓ ░▒▓░
+     ▒ ░ ░    ▒   ▒▒ ░░ ░ ▒  ░░ ░ ▒  ░ ░ ░  ░   ░       ░  ░      ░ ▒ ░░ ░░   ░ ▒░ ░ ░  ░  ░▒ ░ ▒░
+     ░   ░    ░   ▒     ░ ░     ░ ░      ░    ░         ░      ░    ▒ ░   ░   ░ ░    ░     ░░   ░
+      ░          ░  ░    ░  ░    ░  ░   ░  ░                  ░    ░           ░    ░  ░   ░
+"""
+
+    print(banner2)
+
+    print("> process starting please wait")
+    print("5")
+    time.sleep(1)
+    print("4")
+    time.sleep(1)
+    print("3")
+    time.sleep(1)
+    print("2")
+    time.sleep(1)
+    print("1")
+    time.sleep(1)
+
+    time.sleep(1)
+
+    characters = string.ascii_lowercase + string.digits
+    for _ in range(1000000):
+        print(
+            Fore.WHITE + ">[ ] adress : %s I etherum found : 0.OOOOO ETH < " % "".join(random.sample(characters, 32)))
+
+    time.sleep(0.5)
+
+    print(Fore.GREEN + "> %s I 1.23838 ETH" % "".join(random.sample(characters, 32)))
+
+    time.sleep(0.5)
+
+    print(Fore.CYAN + ">Attemping to transfer to your "" wallet...")
+
+    time.sleep(0.5)
+
+    print(Fore.CYAN + "> sending crypto to main adress ")
+
+    time.sleep(0.5)
+
+    print(Fore.CYAN + "> crypto has been succesfully sent to your crypto wallet adress ", address, )
+
+    print(Fore.CYAN + "> you'r now rich !")
+
+    time.sleep(6)
+
+    os.system('cls||clear')
+
+    print("you succeessfully recieve etherum to your wallet")
+
+    time.sleep(1)
+
+    os.system('cls||clear')
+
+    banner3 = """\
+         ______  __ __   ____  ____   __  _  _____     _____   ___   ____       __ __   _____ ____  ____    ____ 
+        |      T|  T  T /    T|    \ |  l/ ]/ ___/    |     | /   \ |    \     |  T  T / ___/l    j|    \  /    T
+        |      ||  l  |Y  o  ||  _  Y|  ' /(   \_     |   __jY     Y|  D  )    |  |  |(   \_  |  T |  _  YY   __j
+        l_j  l_j|  _  ||     ||  |  ||    \ \__  T    |  l_  |  O  ||    /     |  |  | \__  T |  | |  |  ||  T  |
+          |  |  |  |  ||  _  ||  |  ||     Y/  \ |    |   _] |     ||    \     |  :  | /  \ | |  | |  |  ||  l_ |
+          |  |  |  |  ||  |  ||  |  ||  .  |\    |    |  T   l     !|  .  Y    l     | \    | j  l |  |  ||     |
+          l__j  l__j__jl__j__jl__j__jl__j\_j \___j    l__j    \___/ l__j\_j     \__,_j  \___j|____jl__j__jl___,_j
+
+
+    """
+
+    print(banner3)
+
+    input("de you wanna rerun miner : ")
+
+else:
+    print("")
+
+os.system("PAUSE")
+
---
+++ start_WP_tool.bat
@@ -0,0 +1,1 @@
+python WP.py
